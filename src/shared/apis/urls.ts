3db8b0e56fee558f20a43187d17b3c4c2d3aceba
jstar000 <wltjd6300@naver.com //>
2025-05-28 03:06:03 +0000

    fix: API 경로 상수 수정
    - 일부 api 경로 앞에 / 가 붙어있지 않아 api 요청이 절대경로가 아닌 상대경로로 요청되는 문제 수정

--- src/shared/apis/urls.ts
+++ src/shared/apis/urls.ts
@@ -1,15 +1,15 @@
 export const API = {
   AUTH: {
     REQUEST_AUTH_CODE: '/auth/code',
-    VERIFY_AUTH_CODE: 'auth/verification',
-    REMEMBER_AUTH_INFO: 'auth/memory',
-    ADMIN_LOGIN: 'auth/login',
+    VERIFY_AUTH_CODE: '/auth/verification',
+    REMEMBER_AUTH_INFO: '/auth/memory',
+    ADMIN_LOGIN: '/auth/login',
   },
   OPEN_REQUEST: {
     REQUEST_SPACE_OPEN: (spaceId: number) => `/open-requests/${spaceId}`,
   },
   SPACES: {
-    FETCH_SPACE_OPEN_STATUS: 'spaces/open',
+    FETCH_SPACE_OPEN_STATUS: '/spaces/open',
     FETCH_SPACE_RESERVATION_STATUS: '/spaces/reservation',
   },
 };
